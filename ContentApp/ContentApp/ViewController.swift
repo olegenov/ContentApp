7b9a82b544d43b272b958113bdef0dc83a329e4b
olegenov <nik_kitaev@mail.ru>
2024-02-19 23:50:58 +0300

    Project created

---
+++ ContentApp/ContentApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContentApp
+//
+//  Created by Никита Китаев on 19.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
